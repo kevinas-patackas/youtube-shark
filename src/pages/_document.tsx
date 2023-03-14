bbd67bb12342304b0ff2bcff077c53e16e63163b
Kevinas Patackas <kevinas.patackas@railsbank.com>
2023-03-14 18:07:07 +0200

    initial test

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import theme from "@/config/theme";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body
+        style={{
+          backgroundColor: theme.palette.grey[100],
+        }}
+      >
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
